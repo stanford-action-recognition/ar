bfa6abe8a4e58f10ba65e9916a9cf727a356080a
Hongbo Miao <Hongbo.Miao@outlook.com>
2021-05-24 08:54:25 +0800

    chore: update default number of workers to 0 to adapt to different cores of CPU

--- args.py
+++ args.py
@@ -52,7 +52,7 @@ def get_args():
     parser.add_argument(
         "--num_workers",
         type=int,
-        default=8,
+        default=0,
         help="number of workers (default: 0)",
     )
 
